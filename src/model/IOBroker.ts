a947b992bddf5de7783a19cd8f65bd32c3dad94c
STARTIM <timo.stark@msg.group>
2022-04-03 22:16:25 +0200

    bug fixes

--- src/model/IOBroker.ts
+++ src/model/IOBroker.ts
@@ -1,17 +1,30 @@
 import axios from "axios";
 import { CURRENT_SMART_METER_VALUES } from "./AEConversion";
 
+export enum CURRENT_BATTERY_TREND {
+    FAST_LOWER = "FastLower",
+    LOWER = "Lower",
+    CONSTANT = "Constant",
+    HIGHER = "Higher",
+    FAST_HIGHER = "FastHigher"
+};
+
+export enum CURRENT_INJECTION_MODE  {
+    CONSTANT_INJECTION_LOW = "Constant-Low",
+    CONSTANT_INJECTION_HIGH = "Constant-High",
+    DYNAMIC_INJECTION = "Dynamic"
+};
 export interface CURRENT_IO_BROKER_VALUES {
     batteryStateOfCharge: number;
     solarPower: number;
     batteryPower: number;
     dcPower: number;
     overallUsedPower: number;
-    currentAvg: number;
-    currentCount: number;
     currentDay: number;
     totalKwh: number;
     currentDayWh: number;
+    batteryTrend ?: CURRENT_BATTERY_TREND;
+    injectionMode ?: CURRENT_INJECTION_MODE;
 }
 
 export async function getIOBrokerValues() : Promise<CURRENT_IO_BROKER_VALUES> {
@@ -48,8 +61,6 @@ export async function getIOBrokerValues() : Promise<CURRENT_IO_BROKER_VALUES> {
         batteryPower: batteryCurrent * batteryVoltage,
         dcPower: additionalDcOutput,
         overallUsedPower: overallUsage,
-        currentAvg: currentAvg,
-        currentCount: currentCount,
         currentDay: currentDay,
         totalKwh: totalKwh,
         currentDayWh: currentDayWh
@@ -61,31 +72,58 @@ interface KEY_MAP {
     value: string | number;
 }
 
-export async function updateIOBrokerValues(curAEConversionValues: CURRENT_SMART_METER_VALUES, curValSmartMeter: CURRENT_IO_BROKER_VALUES ) {
+let whHistory : { [hour: number] : { [ minute: number]: number } } = {};
 
+export function fillWhHistory(curAEConversionValues: CURRENT_SMART_METER_VALUES, curValSmartMeter: CURRENT_IO_BROKER_VALUES) {
+    const overAllConsumption = Math.round(curValSmartMeter.overallUsedPower + curAEConversionValues.currentPower);
+    const usedPower = Math.round(curAEConversionValues.currentPower > overAllConsumption ? overAllConsumption : curAEConversionValues.currentPower);
+    const date = new Date();
+
+    if(!whHistory[date.getHours()] ) {
+        whHistory[date.getHours()]  = {};
+    }
+    whHistory[date.getHours()][date.getMinutes()] = usedPower;
+
+}
+
+export async function updateIOBrokerValues(curAEConversionValues: CURRENT_SMART_METER_VALUES, curValSmartMeter: CURRENT_IO_BROKER_VALUES ) {
     let allValues : KEY_MAP[] = [];
 
     const overAllConsumption = Math.round(curValSmartMeter.overallUsedPower + curAEConversionValues.currentPower);
     const usedPower = Math.round(curAEConversionValues.currentPower > overAllConsumption ? overAllConsumption : curAEConversionValues.currentPower);
     const date = new Date();
+
+    //get current hour saved on the backend - if our hour changed, we can push our current value foreward (if that makes sense?)
     const dateStr = date.toLocaleDateString('en-GB').split('/').reverse().join('');
     if (curValSmartMeter.currentDay.toString() !== dateStr) {
-        if (curValSmartMeter.currentCount !== 0) {
-
-        }
-        curValSmartMeter.currentAvg = 0;
-        curValSmartMeter.currentCount = 0;
+        whHistory = {};
 
         //update current 
         curValSmartMeter.totalKwh = curValSmartMeter.totalKwh + curValSmartMeter.currentDayWh / 1000;
-        allValues.push({ key: "0_userdata.0.ae_internal_measure_day", value: dateStr });
+        allValues.push({ key: "0_userdata.0.house_total_used_kwh", value: dateStr });
+        allValues.push( {key: "0_userdata.0.ae_internal_total_kwh", value: curValSmartMeter.totalKwh });
     }
-    curValSmartMeter.currentAvg = ((curValSmartMeter.currentAvg * curValSmartMeter.currentCount) + usedPower) / (curValSmartMeter.currentCount + 1);
-    curValSmartMeter.currentCount += 1;
-    const injectionKwH = Math.round(curValSmartMeter.currentAvg * date.getHours()) + (curValSmartMeter.currentAvg * date.getMinutes() / 60, 0);
+
+    //calc value per hour..
+    let injectionKwH = 0;
+    Object.getOwnPropertyNames(whHistory).forEach((hour) => {
+        let kwInHour = 0;
+        let kwLastMinute = -1;
+        Object.getOwnPropertyNames(whHistory[hour]).forEach((minuteStr) => {
+            const minute = parseInt(minuteStr);
+            kwInHour = kwInHour + whHistory[hour][minute] * ( minute - kwLastMinute );
+            kwLastMinute = minute;
+        });
+
+        injectionKwH += kwInHour / 60;
+    });
+
+    injectionKwH = Math.round(injectionKwH);
+
     const totalKwhTotal = injectionKwH / 1000 + curValSmartMeter.totalKwh;
 
-
+    allValues.push({ key: "0_userdata.0.battery_trend", value: curValSmartMeter.batteryTrend });
+    allValues.push({ key: "0_userdata.0.house_injection_mode", value: curValSmartMeter.injectionMode });
     allValues.push({ key: "0_userdata.0.ae_conversion_0_power", value: curAEConversionValues.currentPower});
     allValues.push({ key: "0_userdata.0.ae_conversion_0_limit", value: curAEConversionValues.currentReduce});
     allValues.push({ key: "0_userdata.0.ae_conversion_0_efficency", value: curAEConversionValues.currentEfficiency * 100});
@@ -93,11 +131,9 @@ export async function updateIOBrokerValues(curAEConversionValues: CURRENT_SMART_
     allValues.push({ key: "0_userdata.0.house_consumption_bigger_zero", value: curValSmartMeter.overallUsedPower > 0 ? curValSmartMeter.overallUsedPower : 0});
     allValues.push({ key: "0_userdata.0.house_real_consumption", value: overAllConsumption});
     allValues.push({ key: "0_userdata.0.house_used_consumption", value: usedPower});
-    allValues.push({ key: "0_userdata.0.ae_internal_measure_used_avg", value: curValSmartMeter.currentAvg});
-    allValues.push({ key: "0_userdata.0.ae_internal_measure_count", value: curValSmartMeter.currentCount});
     allValues.push({ key: "0_userdata.0.house_current_day_used_kwh", value: injectionKwH});
     allValues.push({ key: "0_userdata.0.house_total_savings", value: totalKwhTotal * 0.33});
-    allValues.push({ key: "0_userdata.0.house_total_used_kwh", value: totalKwhTotal});
+    allValues.push({ key: "0_userdata.0.house_total_used_kwh", value: totalKwhTotal });
 
     await axios.get(`http://192.168.178.81:8087/setBulk?${ allValues.map((e) => `${e.key}=${e.value}`).join("&") }`);
 }
--- src/model/MainLoop.ts
+++ src/model/MainLoop.ts
@@ -1,66 +1,221 @@
-import { getAEConversionData, reduceAEConversion } from "./AEConversion";
-import { getIOBrokerValues, updateIOBrokerValues } from "./IOBroker";
+import { CURRENT_SMART_METER_VALUES, getAEConversionData, reduceAEConversion } from "./AEConversion";
+import { CURRENT_BATTERY_TREND, CURRENT_INJECTION_MODE, CURRENT_IO_BROKER_VALUES, fillWhHistory, getIOBrokerValues, updateIOBrokerValues } from "./IOBroker";
 
 
 let lastReduceTimestamp = 0;
 let counterMainIteration = 0;
+let injectionMode = CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+let batteryTrend = CURRENT_BATTERY_TREND.CONSTANT;
+let batteryStateOfChargeHistory : { [key: number] : number } = {};
+const overallMaxValue = 500;
+
 
 export async function mainLoopIteration() {
     let [ curValSmartMeter, curAEConversionValues] = await Promise.all([getIOBrokerValues(), getAEConversionData()]);
 
-    
+    batteryTrend = checkTrends(curValSmartMeter);
+    curValSmartMeter.batteryTrend = batteryTrend;
+
+    injectionMode = controlInjectionMode(curValSmartMeter);
+    curValSmartMeter.injectionMode = injectionMode;
+
+    fillWhHistory(curAEConversionValues, curValSmartMeter);
+
     try {
-        //update iobroker every 5 seconds only
         if ( counterMainIteration % 5 === 0 ) {
             await updateIOBrokerValues(curAEConversionValues, curValSmartMeter);
         }
     } catch (err) {  }
+    counterMainIteration ++;
 
+
+    //get possible low and high value based on our injection mode..
+    const limits = determineCurrentLimits( curAEConversionValues, curValSmartMeter );
     const curDate = new Date().getTime() / 1000;
 
-    //we do not want moe changes than every 60 seconds as aeconversion simply takes a while to react..
-    if ( curDate - lastReduceTimestamp < 60 ) {
+    let newDynamicValue = curAEConversionValues.currentReduce;
+    
+    //is our current value unplausible? we are probably inside a change atm.. skip it..
+    if ( curAEConversionValues.currentPower > curAEConversionValues.currentReduce * 1.02 ) {
+        return;
+    } else if ( curAEConversionValues.currentPower < curAEConversionValues.currentReduce * 0.8 ) {
         return;
     }
 
-    if ( curValSmartMeter.batteryStateOfCharge < 15 ) {
-        //stop aeconversion - we do not want to clean up the battery
-        if ( curAEConversionValues.currentReduce > 0 ) {
-            console.log(`Battery has only ${ curValSmartMeter.batteryStateOfCharge }% left, therefore shutting down..` );
-
-            return mainLoopReduce(0,0);
-        }
-    } else if ( curValSmartMeter.overallUsedPower > -20 ) {
+    if ( curValSmartMeter.overallUsedPower > -20 ) {
         //we have to increase our inverter power..
         let bNewReduceValue = curAEConversionValues.currentPower + curValSmartMeter.overallUsedPower + 50;
-        if ( bNewReduceValue > 500 ) {
-            bNewReduceValue = 500;
+        if ( bNewReduceValue > overallMaxValue ) {
+            bNewReduceValue = overallMaxValue;
         }
         if ( bNewReduceValue > curAEConversionValues.currentReduce ) {
-            console.log(`Increase from ${ curAEConversionValues.currentReduce }W to ${ bNewReduceValue }W, because overall used power is ${ curValSmartMeter.overallUsedPower }W` );
-
-            return mainLoopReduce(0, bNewReduceValue);
+            newDynamicValue = bNewReduceValue;
         }
     } else if ( curValSmartMeter.overallUsedPower < -75 ) {
         //we have to decrease our inverter power..
         let bNewReduceValue = curAEConversionValues.currentPower + curValSmartMeter.overallUsedPower + 50;
-        if ( bNewReduceValue > 500 ) {
-            bNewReduceValue = 500;
+        if ( bNewReduceValue > overallMaxValue ) {
+            bNewReduceValue = overallMaxValue;
         } else if ( bNewReduceValue < 30 ) {
             bNewReduceValue = 30;
         }
 
         if ( bNewReduceValue < curAEConversionValues.currentReduce ) {
-            console.log(`Reduce from ${ curAEConversionValues.currentReduce }W to ${ bNewReduceValue }W, because overall used power is ${ curValSmartMeter.overallUsedPower }W` );
-            return mainLoopReduce(0, bNewReduceValue);
+            newDynamicValue = bNewReduceValue;
         }
     }
+
+    if ( newDynamicValue > limits.max ) {
+        newDynamicValue = limits.max;
+    } else if ( newDynamicValue < limits.min ) {
+        newDynamicValue = limits.min;
+    }
+
+    //we go up higher immediatly - lower we wait ~~20 seconds, just because the system is not behaving perfectly.. maybe we already jump up agan..
+    if ( newDynamicValue > curAEConversionValues.currentReduce ) {
+        await mainLoopReduce( 0, newDynamicValue);
+    } else if ( newDynamicValue < curAEConversionValues.currentReduce ) {
+        if(!lastReduceTimestamp) {
+            lastReduceTimestamp = new Date().getTime() / 1000;
+        }
+        
+        if ( curDate - lastReduceTimestamp > 20 ) {
+            await mainLoopReduce( 0, newDynamicValue);
+            lastReduceTimestamp = null;
+        }
+    }
+    
     return true;
 }
 
 async function mainLoopReduce(inverter: number, newVal: number) {
-    lastReduceTimestamp = new Date().getTime() / 1000;
-    counterMainIteration ++;
-
     return reduceAEConversion(inverter, newVal);
+}
+
+function getBatteryFromBefore(minTarget: number, fallback: number) {
+    if ( minTarget < 0 ) {
+        minTarget = 59 + minTarget;
+    }
+    if(!batteryStateOfChargeHistory[minTarget]) {
+        return fallback;
+    }
+    return batteryStateOfChargeHistory[minTarget];
+}
+
+function checkTrends(curValSmartMeter: CURRENT_IO_BROKER_VALUES) {
+    const curMin = new Date().getMinutes();
+    batteryStateOfChargeHistory[curMin] = curValSmartMeter.batteryStateOfCharge;
+
+    //get values before..
+    const before15 = getBatteryFromBefore(curMin - 15, curValSmartMeter.batteryStateOfCharge);
+    const before30 = getBatteryFromBefore(curMin - 30, curValSmartMeter.batteryStateOfCharge);
+    const before45 = getBatteryFromBefore(curMin - 45, curValSmartMeter.batteryStateOfCharge);
+
+    if ( before15 < curValSmartMeter.batteryStateOfCharge && before30 < before15 && before45 < before30 ) {
+        return CURRENT_BATTERY_TREND.FAST_HIGHER;
+    } else if ( before45 < curValSmartMeter.batteryStateOfCharge ) {
+        return CURRENT_BATTERY_TREND.HIGHER;
+    } else if ( before45 == curValSmartMeter.batteryStateOfCharge ) {
+        return CURRENT_BATTERY_TREND.CONSTANT;
+    } else if ( before45 > before30 && before30 > before15 && before15 > curValSmartMeter.batteryStateOfCharge ) {
+        return CURRENT_BATTERY_TREND.FAST_LOWER;
+    } else if ( before45 > curValSmartMeter.batteryStateOfCharge ) {
+        return CURRENT_BATTERY_TREND.LOWER;
+    }
+    
+    return CURRENT_BATTERY_TREND.CONSTANT;
+}
+
+function controlInjectionMode(curValSmartMeter: CURRENT_IO_BROKER_VALUES) : CURRENT_INJECTION_MODE {
+    if ( injectionMode === CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH ) {
+        if ( curValSmartMeter.batteryStateOfCharge < 20 ) {
+            //stop - whatever our direction is - we are putting everything in the net, with less than 20% battery state..
+            if ( batteryTrend === CURRENT_BATTERY_TREND.CONSTANT || batteryTrend === CURRENT_BATTERY_TREND.FAST_LOWER || batteryTrend === CURRENT_BATTERY_TREND.LOWER ) {
+                return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW;
+            }
+            return CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+        } else if ( curValSmartMeter.batteryStateOfCharge < 50 && batteryTrend === CURRENT_BATTERY_TREND.LOWER ) {
+            return CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+        } else if ( curValSmartMeter.batteryStateOfCharge < 75 && batteryTrend === CURRENT_BATTERY_TREND.FAST_LOWER ) {
+            return CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+        }
+        
+        return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH;
+    }
+
+    if ( injectionMode === CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW ) {
+        if ( curValSmartMeter.batteryStateOfCharge > 20 && ( batteryTrend === CURRENT_BATTERY_TREND.FAST_HIGHER || batteryTrend === CURRENT_BATTERY_TREND.HIGHER ) ) {
+            return CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+        }
+        return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW;
+    }
+
+    if ( injectionMode === CURRENT_INJECTION_MODE.DYNAMIC_INJECTION ) {
+        if ( curValSmartMeter.batteryStateOfCharge < 20 ) {
+            return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW;
+        }
+        if ( curValSmartMeter.batteryStateOfCharge > 40 && batteryTrend === CURRENT_BATTERY_TREND.FAST_HIGHER ) {
+            return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH;
+        }
+        if ( curValSmartMeter.batteryStateOfCharge > 80 && batteryTrend === CURRENT_BATTERY_TREND.HIGHER ) {
+            return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH;
+        }
+        if ( curValSmartMeter.batteryStateOfCharge > 90 ) {
+            return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH;
+        }
+        return CURRENT_INJECTION_MODE.DYNAMIC_INJECTION;
+    }
+
+    return CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW;
+}
+
+
+function determineCurrentLimits(curAEConversionValues: CURRENT_SMART_METER_VALUES, curValSmartMeter: CURRENT_IO_BROKER_VALUES) {
+    if ( curValSmartMeter.injectionMode === CURRENT_INJECTION_MODE.CONSTANT_INJECTION_HIGH ) {
+        return {
+            min: overallMaxValue,
+            max: overallMaxValue
+        }
+    }
+    
+    if ( curValSmartMeter.injectionMode === CURRENT_INJECTION_MODE.CONSTANT_INJECTION_LOW ) {
+        if ( curValSmartMeter.batteryStateOfCharge > 50 ) {
+            return {
+                min: 200,
+                max: 200
+            };
+        } else if ( curValSmartMeter.batteryStateOfCharge > 30 ) {
+            return {
+                min: 175,
+                max: 175
+            }
+        } else if (curValSmartMeter.batteryStateOfCharge > 20 ) {
+            return {
+                min: 150,
+                max: 150
+            }
+        } else if ( curValSmartMeter.batteryStateOfCharge > 15 ) {
+            return {
+                min: 100,
+                max: 100
+            }
+        } else if ( curValSmartMeter.batteryStateOfCharge > 10 ) {
+            return {
+                min: 50,
+                max: 50
+            }
+        } else {
+            return {
+                min: 0,
+                max: 0,
+            }
+        }
+    }
+    
+    if ( curValSmartMeter.injectionMode === CURRENT_INJECTION_MODE.DYNAMIC_INJECTION ) {
+        return {
+            min: 100,
+            max: overallMaxValue
+        }
+    }
 }
